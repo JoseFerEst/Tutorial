230b8b089ab334070542329e7962bb6fe66fc5f2
Usuario <Usuario@DESKTOP-8MGRS62>
2025-10-05 20:38:14 +0200

    Nodo 10 - Descontinuado

--- src/tutorial/Tutorial.java
+++ src/tutorial/Tutorial.java
@@ -5,7 +5,7 @@ public class Tutorial {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hola CMEPPS");
-		System.out.println("Rama Descontinuada (8)");
+		System.out.println("Rama Descontinuada (10)");
 	}
 
 }
