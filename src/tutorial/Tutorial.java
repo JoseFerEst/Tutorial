048a03a4e97e81d82f32c03f0f0059fa74f9a05a
Usuario <Usuario@DESKTOP-8MGRS62>
2025-10-05 20:37:37 +0200

    Nodo 8

--- src/tutorial/Tutorial.java
+++ src/tutorial/Tutorial.java
@@ -5,7 +5,7 @@ public class Tutorial {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hola CMEPPS");
-		System.out.println("Rama Descontinuada (5)");
+		System.out.println("Rama Descontinuada (8)");
 	}
 
 }
