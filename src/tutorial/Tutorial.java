e2468d7af52bc17d8e9f421559bb925426f382fe
Usuario <Usuario@DESKTOP-8MGRS62>
2025-10-05 20:45:50 +0200

    Nodo 7

--- src/tutorial/Tutorial.java
+++ src/tutorial/Tutorial.java
@@ -5,7 +5,7 @@ public class Tutorial {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hola CMEPPS");
-		System.out.println("Nodo 6");
+		System.out.println("Nodo 7");
 	}
 
 }
